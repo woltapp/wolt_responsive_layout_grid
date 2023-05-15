dd89c575698f7c28a2e9c1b964831b7806e4306f
Cagatay Ulusoy <cagatay.ulusoy@wolt.com>
2023-05-15 16:22:01 +0300

    rename mock names

--- example/lib/entities/mock_coffee_orders.dart
+++ example/lib/entities/mock_coffee_orders.dart
@@ -2,13 +2,13 @@ import 'package:wolt_responsive_layout_grid_example/entities/coffee_maker_step.d
 import 'package:wolt_responsive_layout_grid_example/entities/coffee_order.dart';
 
 const List<CoffeeOrder> mockCoffeeOrders = <CoffeeOrder>[
-  CoffeeOrder(coffeeMakerStep: CoffeeMakerStep.grind, orderName: 'Cagatay U.', id: '#001'),
-  CoffeeOrder(coffeeMakerStep: CoffeeMakerStep.grind, orderName: 'Ege A.', id: '#002'),
-  CoffeeOrder(coffeeMakerStep: CoffeeMakerStep.grind, orderName: 'Yunus B.', id: '#003'),
-  CoffeeOrder(coffeeMakerStep: CoffeeMakerStep.grind, orderName: 'Murat T.', id: '#004'),
-  CoffeeOrder(coffeeMakerStep: CoffeeMakerStep.grind, orderName: 'Ali R.', id: '#005'),
-  CoffeeOrder(coffeeMakerStep: CoffeeMakerStep.grind, orderName: 'Antti R.', id: '#006'),
-  CoffeeOrder(coffeeMakerStep: CoffeeMakerStep.addWater, orderName: 'Otto O.', id: '#007'),
-  CoffeeOrder(coffeeMakerStep: CoffeeMakerStep.addWater, orderName: 'Gizem K.', id: '#008'),
-  CoffeeOrder(coffeeMakerStep: CoffeeMakerStep.ready, orderName: 'Raza P.', id: '#009'),
+  CoffeeOrder(coffeeMakerStep: CoffeeMakerStep.grind, orderName: 'Yuho W.', id: '#001'),
+  CoffeeOrder(coffeeMakerStep: CoffeeMakerStep.grind, orderName: 'John Doe', id: '#002'),
+  CoffeeOrder(coffeeMakerStep: CoffeeMakerStep.grind, orderName: 'Jane Smith', id: '#003'),
+  CoffeeOrder(coffeeMakerStep: CoffeeMakerStep.grind, orderName: 'Michael Johnson', id: '#004'),
+  CoffeeOrder(coffeeMakerStep: CoffeeMakerStep.grind, orderName: 'Sarah Davis', id: '#005'),
+  CoffeeOrder(coffeeMakerStep: CoffeeMakerStep.grind, orderName: 'David Wilson', id: '#006'),
+  CoffeeOrder(coffeeMakerStep: CoffeeMakerStep.addWater, orderName: 'Emily Brown', id: '#007'),
+  CoffeeOrder(coffeeMakerStep: CoffeeMakerStep.addWater, orderName: 'Robert Jones', id: '#008'),
+  CoffeeOrder(coffeeMakerStep: CoffeeMakerStep.ready, orderName: 'Jennifer Taylor', id: '#009'),
 ];
